1615f8f2541a1d65e78fb8dd25f049289ad35e6d
Hamza <hamzatarar8@gmail.com>
2022-04-02 13:20:21 +0200

    Corrected linter errors.

--- Gemfile
+++ Gemfile
@@ -72,4 +72,3 @@ group :test do
   gem 'selenium-webdriver'
   gem 'webdrivers'
 end
-gem "rspec-rails", group: [:development, :test]
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,17 +1,17 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "Posts", :type => :request do
-  it "redirects and renders to the Posts page" do
-    get "/users/:user_id/posts"
+RSpec.describe 'Posts', type: :request do
+  it 'redirects and renders to the Posts page' do
+    get '/users/:user_id/posts'
     expect(response).to render_template(:index)
     expect(response).to have_http_status(:ok)
-    expect(response.body).to include("Posts for a given user.")
+    expect(response.body).to include('Posts for a given user.')
   end
 
-  it "redirects and renders to the specific post" do
-    get "/users/:user_id/posts/:id"
+  it 'redirects and renders to the specific post' do
+    get '/users/:user_id/posts/:id'
     expect(response).to render_template(:show)
     expect(response).to have_http_status(:ok)
-    expect(response.body).to include("The specific post")
+    expect(response.body).to include('The specific post')
   end
 end
