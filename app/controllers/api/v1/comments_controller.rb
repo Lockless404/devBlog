8fb7863d370b1c110ec1f30aeeafe17897fe8697
Hamza <hamzatarar8@gmail.com>
2022-04-30 07:47:09 +0200

    Added api for posts and comments, authorization for user.

---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,23 @@
+class Api::V1::CommentsController < ApplicationController
+  def index
+    @post = Post.find(params[:post_id])
+    @comments = @post.comments.all
+    render json: @comments
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:post_id])
+    @comment = @user.comments.new(post_id: @post.id, author_id: @user.id, text: params[:text])
+    @comment.post_id = @post.id
+    respond_to do |format|
+      format.json do
+        if @comment.save
+          render json: { success: true, message: 'Comment was successfully created.' }, status: :ok
+        else
+          render json: { success: false, message: 'Comment was not created.' }, status: :unprocessable_entity
+        end
+      end
+    end
+  end
+end
